31f18a5d1e7557f8c3af25fee3bed528e1d5aa30
C0ppens <45225697+C0ppens@users.noreply.github.com>
2021-07-11 22:32:46 +1000

    fixed contact page css bug

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,17 +3,16 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import '../components/layout.css'
 
 const ContactPage = () => (
-  <div className="dark" style={{height: "100%"}}>
   <Layout>
     <Seo title="Contact" />
    
-    <h1 style={{margin: `1rem`}}>Contact</h1>
-    <p style={{margin: `1rem`}}>Email: <a href="mailto:adam-coppens@outlook.com">adam-coppens@outlook.com</a></p>
-    <p style={{margin: `1rem`}}>LinkedIn: <a href="https://www.linkedin.com/in/adam-coppens-a44956190/">https://www.linkedin.com/in/adam-coppens-a44956190/</a></p>
+    <h1 style={{margin: `2rem`}}>Contact</h1>
+    <p style={{margin: `2rem`}}>Email: <a href="mailto:adam-coppens@outlook.com">adam-coppens@outlook.com</a></p>
+    <p style={{margin: `2rem`}}>LinkedIn: <a href="https://www.linkedin.com/in/adam-coppens-a44956190/"></a></p>
   </Layout>
-  </div>
 )
 
 export default ContactPage
