5a7f295745948dfd846a14a24baee7ad6b68f307
C0ppens <45225697+C0ppens@users.noreply.github.com>
2022-06-17 23:52:24 +1000

    Updates

---
+++ component---src-pages-blog-js-e289375ee8f9f984dd73.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[7],{223:function(e,t,r){"use strict";r.r(t);var l=r(7294),a=r(4832),n=r(3751);t.default=function(){return l.createElement(a.Z,null,l.createElement(n.Z,{title:"Blog"}),l.createElement("h1",{style:{margin:"2rem"}},"Blog"),l.createElement("p",{style:{margin:"2rem"}},"Welcome to my Personal Blog. Here you will find tech musings, Work from home tips and more!"))}}}]);
+//# sourceMappingURL=component---src-pages-blog-js-e289375ee8f9f984dd73.js.map
